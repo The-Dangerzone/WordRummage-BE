e4d7601757ac67fb2288854fb223ec3540ec4d10
Joe Davitt <joseph.j.davitt@gmail.com>
2023-05-25 22:50:37 +0000

    add user model

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const User = require('./models/user.js');
 
 const app = express();
 app.use(cors());
