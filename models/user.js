d091fca937c39875882b3ed598d6c3220bcbb826
Joe Davitt <joseph.j.davitt@gmail.com>
2023-05-25 23:11:56 +0000

    update user schema

--- models/user.js
+++ models/user.js
@@ -6,9 +6,6 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  list: { type: Array, required: false },
-  pantry: { type: Array, required: false },
 });
 
 const UserSchema = mongoose.model('user', userSchema);
